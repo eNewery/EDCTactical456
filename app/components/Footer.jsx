27ac0f8368729ba41e009530fa5053d64d8fd6ce
eNewery <robdiaz247@gmail.com>
2023-08-04 14:27:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/components/Footer.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React from "react";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+const Footer = () => {
+  const [clickedLink, setClickedLink] = useState("home");
+  useEffect(() => {
+    const biography = document.querySelector(".biography");
+    const courses = document.querySelector(".courses");
+    const home = document.querySelector(".home");
+    const shop = document.querySelector(".shop");
+    const cart = document.querySelector(".cart");
+    if (clickedLink == "biography") {
+      biography.classList.add("borderLinkActive");
+      courses.classList.remove("borderLinkActive");
+      home.classList.remove("borderLinkActive");
+      shop.classList.remove("borderLinkActive");
+      cart.classList.remove("borderLinkActive");
+    }
+    if (clickedLink == "courses") {
+      courses.classList.add("borderLinkActive");
+      biography.classList.remove("borderLinkActive");
+      home.classList.remove("borderLinkActive");
+      shop.classList.remove("borderLinkActive");
+      cart.classList.remove("borderLinkActive");
+    }
+    if (clickedLink == "home") {
+      home.classList.add("borderLinkActive");
+      biography.classList.remove("borderLinkActive");
+      courses.classList.remove("borderLinkActive");
+      shop.classList.remove("borderLinkActive");
+      cart.classList.remove("borderLinkActive");
+    }
+    if (clickedLink == "shop") {
+      shop.classList.add("borderLinkActive");
+      biography.classList.remove("borderLinkActive");
+      courses.classList.remove("borderLinkActive");
+      home.classList.remove("borderLinkActive");
+      cart.classList.remove("borderLinkActive");
+    }
+    if (clickedLink == "cart") {
+      cart.classList.add("borderLinkActive");
+      biography.classList.remove("borderLinkActive");
+      courses.classList.remove("borderLinkActive");
+      home.classList.remove("borderLinkActive");
+      shop.classList.remove("borderLinkActive");
+    }
+    <Head></Head>
+  }, [clickedLink]);
+
+  return (
+    <div className="footerContainer">
+      <p>
+        Contenidos de EDC Tactical protegidos por derechos de autor; uso sin
+        permiso puede ser ilegal. ©
+      </p>
+      <div className="footerMediaLinks">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,100,0,0" />
+        <Link href="/Biography" onClick={() => setClickedLink("biography")}>
+        <span
+          class={`footerMediaLink material-symbols-outlined biography`}
+        >
+          person_book
+        </span>
+        </Link>
+        <Link href="/Courses" onClick={() => setClickedLink("courses")}>
+        <span
+          class={`footerMediaLink material-symbols-outlined courses`}
+        >
+          school
+        </span>
+        </Link>
+        <Link href="/" onClick={() => setClickedLink("home")}>
+        <span
+          class={`footerMediaLink material-symbols-outlined home`}
+        >
+          home
+        </span>
+        </Link>
+        <Link href="/Shop" onClick={() => setClickedLink("shop")}>
+        <span
+          class={`footerMediaLink material-symbols-outlined shop`}
+        >
+          storefront
+        </span>
+        </Link>
+        <Link href="/Cart" onClick={() => setClickedLink("cart")}>
+        <span
+          class={`footerMediaLink material-symbols-outlined cart`}
+        >
+          shopping_cart
+        </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+
+  
+  return (
+    <main className='homeContainer'>
+      <div className="homeLogo"></div>
+      <button className='homeBtn'>Ver Mas</button>
+    </main>
+  )
+}
